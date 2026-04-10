b1e56505fb4d6f64358e2d1875f30d93bf9a71f7
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-04-09 21:02:42 -0500

    Agregando suspense

--- src/routes/Navigations.tsx
+++ src/routes/Navigations.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { BrowserRouter } from "react-router-dom"
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom'
 import { routes } from "./routes"
@@ -6,27 +7,29 @@ import logo from '../logo.svg'
 
 export const Navigations = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React Logo" />
-          <ul>
-            {routes.map(({ name, to }) => (
-              <li key={to}>
-                <NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}>
-                  {name}
-                </NavLink>
-              </li>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map(({ name, to }) => (
+                <li key={to}>
+                  <NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
             ))}
-          </ul>
-        </nav>
-        <Routes>
-          {routes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component />} />
-          ))}
-          <Route path='/*' element={<Navigate to={routes[0].to} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path='/*' element={<Navigate to={routes[0].to} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   )
 }
